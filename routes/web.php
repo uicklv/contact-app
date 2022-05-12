b2f45208ad4de871ac61309fc69b98e56c04ac32
Topor Nickolay <uicklv@gmail.com>
2022-05-12 21:32:31 +0300

    auth fixes / verify/ restore etc

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
-Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
-Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+    Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+    Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+    Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
+    Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
+    Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+});
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
